97c477b679304af5cfb754dff3e186835d294faf
Francis Plant <francisplantcam@gmail.com>
2020-11-01 03:21:58 -0500

    Done.

--- ButtonListener.js
+++ ButtonListener.js
@@ -1,15 +1,16 @@
 const gpio = require('onoff').Gpio;
 const Button_GPIO_Pin = 16;
-const spawn = require('child_process').spawn;
+const child_process = require('child_process');
+
+child_process.spawn('gpio', ['-g', 'mode', '16', 'up']); // Enable the internal pull-up resistor.
 
 let videoBtn = new gpio(Button_GPIO_Pin, 'in', 'falling',
 				{ debounceTimeout: 10 });
 
 videoBtn.watch(function(err, value) {
-	let openVidCmd = '';
-
 	if (err) console.log('Error: ', err, err.stack);
-	openVidCmd = spawn('chromium-browser', ['"/home/pi/videos/correct_horse.mp4"']);
+	console.log("Button pressed; launching video in default browser...");
+	child_process.spawn('chromium-browser', ['--kiosk', 'file:///home/pi/Videos/correct_horse.mp4']);
 });
 
 process.on('SIGINT', function() {
