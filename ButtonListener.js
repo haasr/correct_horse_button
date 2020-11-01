b87cde6ce7ad6363e05870b5161d92c525f413c0
Francis Plant <francisplantcam@gmail.com>
2020-11-01 18:12:49 -0500

    Added auto-close at end of video.

--- ButtonListener.js
+++ ButtonListener.js
@@ -1,6 +1,7 @@
 const gpio = require('onoff').Gpio;
 const Button_GPIO_Pin = 16;
 const child_process = require('child_process');
+const timer = ms => new Promise( res => setTimeout(res, ms));
 
 child_process.spawn('gpio', ['-g', 'mode', '16', 'up']); // Enable the internal pull-up resistor.
 
@@ -9,8 +10,12 @@ let videoBtn = new gpio(Button_GPIO_Pin, 'in', 'falling',
 
 videoBtn.watch(function(err, value) {
 	if (err) console.log('Error: ', err, err.stack);
+
 	console.log("Button pressed; launching video in browser...");
-	child_process.spawn('chromium-browser', ['--kiosk', 'file:///home/pi/Videos/correct_horse.mp4']);
+	spawn = child_process.spawn('chromium-browser',
+					['--kiosk', 'file:///home/pi/Videos/correct_horse.mp4']);
+	// Adjust timer based on time taken to launch and initiate playback + length of video:
+	timer(44000).then(_ => spawn.kill('SIGTERM'));
 });
 
 process.on('SIGINT', function() {
